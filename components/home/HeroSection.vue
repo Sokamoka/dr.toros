61823310f0425fb98d545843fd42dc0e5f8e43e6
Akos Stegner <a.stegner@mito.hu>
2022-04-25 23:33:40 +0200

    Develop sections

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -1,9 +1,7 @@
 <template>
-  <section class="flex items-center h-[660px] bg-gray-50">
-    <div class="container mx-auto">
-      <div class=" text-5xl text-primary">
-        Think legally, act socially.
-      </div>
+  <section class="flex h-[660px] bg-gradient-to-t from-gray-200 to-white items-end">
+    <div class="container mx-auto px-10">
+      <div class="text-5xl text-primary px-16 pb-32">Think legally, act socially.</div>
     </div>
   </section>
 </template>
--- components/home/ResponsibilitySection.vue
+++ components/home/ResponsibilitySection.vue
@@ -2,15 +2,15 @@
   <section id="responsibility" class="">
     <div class="bg-primary">
       <div class="container mx-auto pt-5 px-10">
-        <h1 class="text-white">Responsibility</h1>
-        <div class="grid grid-cols-1 sm:grid-cols-2 sm:px-16">
-          <div class="text-white">„Hitvallásunk szerint gondolnunk kell a közösségre, amelynek részesei vagyunk és amely lehetővé teszi álmaink megvalósítását. Kiállunk a számunkra fontos társadalmi ügyekért. Az elfogadás és empátia fontosságának hangsúlyozása érdekében a Törös Ügyvédi Iroda többek között az InDaHouse, az UCCU és a Romaster Alapítványok munkájának közvetlen támogatásán keresztül járul hozzá a társadalom kisebbségek irányába történő érzékenyítéséhez. Irodánk tevékeny részese a Bridge Budapest Vállalható Üzleti Kultúráért kezdeményezésnek, irodavezető ügyvédünk a szervezet nagyköveteként is aktív szerepet vállal. Ennek elsődleges célja olyan munkahelyek létrehozása hazánkban, ahol a tudás és a teljesítmény a fő érték és ahol a vállalkozások átlátható, tudatos, felelős üzleti magatartásra alapozzák működésüket. Meggyőződésünk szerint az ügyvédeknek különösen nagy felelőssége van abban, hogy a vállalkozásokat és a vállalati vezetőket transzparens, legális és etikus működés felé tereljék.”</div>
+        <h1 class="text-white my-10">Responsibility</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-16 sm:pl-16">
+          <div class="text-white mb-10">„Hitvallásunk szerint gondolnunk kell a közösségre, amelynek részesei vagyunk és amely lehetővé teszi álmaink megvalósítását. Kiállunk a számunkra fontos társadalmi ügyekért. Az elfogadás és empátia fontosságának hangsúlyozása érdekében a Törös Ügyvédi Iroda többek között az InDaHouse, az UCCU és a Romaster Alapítványok munkájának közvetlen támogatásán keresztül járul hozzá a társadalom kisebbségek irányába történő érzékenyítéséhez. Irodánk tevékeny részese a Bridge Budapest Vállalható Üzleti Kultúráért kezdeményezésnek, irodavezető ügyvédünk a szervezet nagyköveteként is aktív szerepet vállal. Ennek elsődleges célja olyan munkahelyek létrehozása hazánkban, ahol a tudás és a teljesítmény a fő érték és ahol a vállalkozások átlátható, tudatos, felelős üzleti magatartásra alapozzák működésüket. Meggyőződésünk szerint az ügyvédeknek különösen nagy felelőssége van abban, hogy a vállalkozásokat és a vállalati vezetőket transzparens, legális és etikus működés felé tereljék.”</div>
           <div>Kép</div>
         </div>
       </div>
     </div>
     <div class="bg-secondary">
-      <div class="container py-10 text-center">Think legally, act socially.</div>
+      <div class="container mx-auto py-10 text-center text-xl sm:text-4xl text-white">Think legally, act socially.</div>
     </div>
   </section>
 </template>
