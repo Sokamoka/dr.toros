465dc767fde4c55c356ede04fd4576ac19d76e64
Akos Stegner <a.stegner@mito.hu>
2022-04-26 08:18:08 +0200

    Develop teams section

--- components/home/TeamSection.vue
+++ components/home/TeamSection.vue
@@ -1,5 +1,23 @@
 <template>
   <section id="teams" class="flex items-center">
-    <div class="container mx-auto">Teams</div>
+    <div class="container mx-auto p-10">
+      <h1 class="mb-10">Teams</h1>
+
+      <div class="px-16">
+        <div class="flex justify-end mb-16">
+          <p class="max-w-2xl text-right text-3xl">
+            „You can do what I cannot do. I can do what you cannot do. Together we can do great things.”
+            <br />Mother Theresa
+          </p>
+        </div>
+
+        <div class="grid grid-cols-1 sm:grid-cols-3">
+          <div class="">Kép</div>
+          <div class="col-span-2 col-end-4">Details</div>
+        </div>
+
+        <div>Selector</div>
+      </div>
+    </div>
   </section>
 </template>
